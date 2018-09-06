b2310dfddf33baf8ef44b765bcb0274d0a4a026e
Jesfery <sean.shanahan@bigpond.com>
2018-09-07 01:42:52 +1000

    Post message

--- listeners/memberPresence.js
+++ listeners/memberPresence.js
@@ -4,13 +4,14 @@ function checkStreaming(oldPresence, newPresence) {
     let newMember = newPresence.member;
 
     if (!newMember) {
-        console.log(newPresence);
         return;
     }
 
     let guild = newMember.guild,
         newActivityType = utils.get(newPresence, 'activity.type'),
-        promise;
+        oldActivityType = utils.get(oldPresence, 'activity.type'),
+        promise,
+        memberWentLive = false;
 
     if (!guild.streamingRole) {
         guild.streamingRole = guild.roles.find(role => role.name === 'now-streaming');
@@ -19,7 +20,20 @@ function checkStreaming(oldPresence, newPresence) {
         }
     }
 
+    let streamingChannel = guild.channels.find(channel => {
+        return channel.name === 'now-streaming' && channel.type === 'text';
+    });
+
+    if (!streamingChannel) {
+        return;
+    }
+
     if (newActivityType === 'STREAMING') {
+        if (oldActivityType !== 'STREAMING') {
+            //let url = utils.get(newPresence, 'activity.url');
+            //streamingChannel.send(newMember.displayName + ' has gone live. Check it out at <' + url + '>');
+            memberWentLive = true;
+        }
         promise = newMember.roles.add(guild.streamingRole);
     } else {
         promise = newMember.roles.remove(guild.streamingRole);
@@ -37,12 +51,12 @@ function checkStreaming(oldPresence, newPresence) {
             } 
         });
 
-        let streamingChannel = guild.channels.find(channel => {
-            return channel.name === 'now-streaming' && channel.type === 'text';
-        });
-
         if (streamingMembersUrl.length > 0) {
-            streamingChannel.setTopic('MultiTwitch link: http://multitwitch.tv/' + streamingMembersUrl.join('/'));
+            let url = 'http://multitwitch.tv/' + streamingMembersUrl.join('/');
+            streamingChannel.setTopic('MultiTwitch link: ' + url);
+            if (memberWentLive) {
+                streamingChannel.send('@here ' + newMember.displayName + ' has gone live at <' + utils.get(newPresence, 'activity.url') + '>\n\nThe updated multitwitch link is <' + url + '>');
+            }
         } else {
             streamingChannel.setTopic('Theres nobody streaming...');
         }
