b8478c64eff83295c17f445b0d2cba63c59f0b69
Jesfery <sean.shanahan@bigpond.com>
2020-01-24 13:52:54 +1100

    Cooldowns
    * Added 4 hour cooldown on pings
    * Added initialise check in ready handler as safeguard against multiple ready events

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/**
- * Why did they do it?!?
- */
 const Discord = require('discord.js');
 const { token } = require('./config.json');
 
@@ -8,11 +5,19 @@ const commandListener = require('./listeners/command.js');
 const memberPresenceListener = require('./listeners/memberPresence.js');
 
 const client = new Discord.Client();
+const initialised = [];
 
 client.once('ready', () => {
+    //I suspect this event is happening more than once.
+    // Below is added as a safeguard.
+    if (initialised.length > 0) {
+        return;
+    }
+    initialised.push(true);
+
     commandListener.init(client);
     memberPresenceListener.init(client);
-    console.log('Ready!');
+    console.log('Ready at ' + new Date());
 });
 
 client.login(token);
--- listeners/memberPresence.js
+++ listeners/memberPresence.js
@@ -1,4 +1,7 @@
 const utils = require('../utils.js');
+const Discord = require('discord.js');
+const cooldowns = new Discord.Collection();
+const cooldownAmount = 4 * 60 * 60 * 1000; //4 hours. May make this configurable in the future.
 
 function checkStreaming(oldPresence, newPresence) {
     let newMember = newPresence.member;
@@ -40,20 +43,28 @@ function checkStreaming(oldPresence, newPresence) {
         guild.streamingRole.members.forEach(member => {
             let activityUrl = utils.get(member, 'presence.activity.url');
 
-            if (activityUrl && activityUrl.length > 0) {
+            if (activityUrl && activityUrl.length > 0 && activityUrl.indexOf('https://www.twitch.tv/') === 0) {
                 activityUrl = activityUrl.split('/');
                 activityUrl = activityUrl[activityUrl.length -1];
                 streamingMembersUrl.push(activityUrl);
             } 
         });
 
+        const now = Date.now();
+        const memberId = newMember.id;
+        if (newActivityType === 'STREAMING' && oldActivityType !== 'STREAMING' && !cooldowns.has(memberId)) {
+            cooldowns.set(memberId, now);
+            setTimeout(() => cooldowns.delete(memberId), cooldownAmount);
+            let message = '@here \n\n' + newMember.displayName + ' has gone live at <' + utils.get(newPresence, 'activity.url') + '>';
+            if (streamingMembersUrl.length > 0) {
+                message += '\n\nThe updated MultiTwitch link is <' + url + '>';
+            }
+            streamingChannel.send(message);
+        }
+
         if (streamingMembersUrl.length > 0) {
             let url = 'http://multitwitch.tv/' + streamingMembersUrl.join('/');
             streamingChannel.setTopic('MultiTwitch link: ' + url);
-
-            if (newActivityType === 'STREAMING' && oldActivityType !== 'STREAMING') {
-                streamingChannel.send('@here ' + newMember.displayName + ' has gone live at <' + utils.get(newPresence, 'activity.url') + '>\n\nThe updated multitwitch link is <' + url + '>');
-            }
         } else {
             streamingChannel.setTopic('Theres nobody streaming...');
         }
